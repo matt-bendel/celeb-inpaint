ee4aa08023f79d01c0e7fd8a64f81f1c88d8d563
Matt Bendel <bendel.8@osu.edu>
2022-05-07 19:42:34 -0400

    added training plots and debugging statement

--- data_loaders/prepare_data.py
+++ data_loaders/prepare_data.py
@@ -49,7 +49,7 @@ class DataTransform:
 
 
 def create_datasets(args):
-    transform = transforms.Compose([DataTransform(args), transforms.ToTensor()])
+    transform = transforms.Compose([transforms.ToTensor(), DataTransform(args)])
     dataset = datasets.ImageFolder('/storage/celebA-HQ/celeba_hq_128', transform=transform)
     train_data, dev_data, test_data = torch.utils.data.random_split(
         dataset, [24000, 4000, 2000],
