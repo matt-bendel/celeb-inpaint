7266a957ab81206a8cc5f0a58cb14909b354f89a
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-09-07 12:43:16 -0400

    Updated a ton

--- data_loaders/prepare_data.py
+++ data_loaders/prepare_data.py
@@ -34,6 +34,7 @@ class DataTransform:
         plt.savefig(f'mask_{self.args.R}.png')
         plt.close()
         self.mask = torch.tensor(np.reshape(arr, (128, 128)), dtype=torch.float).repeat(3, 1, 1)
+        torch.save(self.mask, 'mast.pt')
 
     def __call__(self, gt_im):
         mean = torch.tensor([0.5, 0.5, 0.5])
