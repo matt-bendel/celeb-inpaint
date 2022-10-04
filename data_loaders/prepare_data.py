e72b59d39f95650a96999e741df2730db5a8f0b0
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-10-04 01:07:37 -0400

    Code to sample with langevin dynamics

--- data_loaders/prepare_data.py
+++ data_loaders/prepare_data.py
@@ -64,7 +64,8 @@ class DataTransform:
 
 def create_datasets(args):
     # transform = transforms.Compose([transforms.ToTensor(), DataTransform(args)])
-    dataset = datasets.ImageFolder('/storage/celebA-HQ/celeba_hq_128')
+    transform = transforms.Compose([DataTransform(args)])
+    dataset = datasets.ImageFolder('/storage/celebA-HQ/celeba_hq_128', transform=transform)
     train_data, dev_data, test_data = torch.utils.data.random_split(
         dataset, [27000, 2000, 1000],
         generator=torch.Generator().manual_seed(0)
