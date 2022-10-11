bf3bf13c11cef066b411996ba1cc11e127dd9cb9
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-10-11 17:14:36 -0400

    Added call to new LPIPS metric

--- evaluation_scripts/lpips/lpips_metric.py
+++ evaluation_scripts/lpips/lpips_metric.py
@@ -31,7 +31,7 @@ class LPIPSMetric:
                 embedImg1 = torch.zeros(size=(img1.size(0), 3, 128, 128)).cuda()
                 embedImg2 = torch.zeros(size=(img2.size(0), 3, 128, 128)).cuda()
 
-                for i in range(inp.size(0)):
+                for i in range(img1.size(0)):
                     im1 = img1[i, :, :, :] * std[i, :, None, None] + mean[i, :, None, None]
                     im1 = 2 * (im1 - torch.min(im1)) / (torch.max(im1) - torch.min(im1)) - 1
                     embedImg1[i, :, :, :] = im1
