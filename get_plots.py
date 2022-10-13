32519812c9cf6759a64648ce3d3c42cbcb672922
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-10-13 09:58:36 -0400

    Implementation of value iteration and every-visit monte carlo

--- get_plots.py
+++ get_plots.py
@@ -226,4 +226,4 @@ if __name__ == '__main__':
         truncation_latent = torch.mean(G.get_mean_code_vector(y, x, mask, num_latents=128), dim=0)
         break
 
-    get_plots(args, G_ours, G_comod, test_loader, None, truncation_latent)
+    get_plots(args, G_ours, G_comod, test_loader, 0.25, truncation_latent)
