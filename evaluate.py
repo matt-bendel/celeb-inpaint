47df89f7f601680af61d5c68ec5970db3ecfc64f
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-10-28 02:30:39 -0400

    Changes

--- evaluate.py
+++ evaluate.py
@@ -230,7 +230,7 @@ if __name__ == '__main__':
     # print(f'CFID PINV: {cfid_pinv}')
 
     # vals = [1, 2, 4, 8, 16, 32]
-    # get_fid(args, G, test_loader, train_loader, t=None, truncation_latent=None)
+    get_fid(args, G, test_loader, train_loader, t=None, truncation_latent=None)
     get_cfid(args, G, test_loader, 32, None, None)
     get_cfid(args, G, test_loader, 8, val_loader, None)
     get_cfid(args, G, test_loader, 1, val_loader, train_loader)
