ddedd5c7db7f3230eef94ec3784f94e85401a399
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-03-28 20:00:54 -0400

    Redoing plots...

--- get_plots.py
+++ get_plots.py
@@ -114,19 +114,19 @@ def get_plots(args, G_ours, G_comod, test_loader, truncation, truncation_latent)
             for j in range(y.size(0)):
                 total += 1
 
-                if total in best_ssims:
+                if total in best_lpips:
                     fig_count += 1
 
                     fig = plt.figure()
                     plt.axis('off')
                     plt.imshow(x[j, :, :, :].cpu().numpy().transpose(1, 2, 0))
-                    plt.savefig(f'neurips_plots/ssim/original_{fig_count}.png', bbox_inches='tight', dpi=300)
+                    plt.savefig(f'neurips_plots/lpips/original_{fig_count}.png', bbox_inches='tight', dpi=300)
                     plt.close(fig)
 
                     fig = plt.figure()
                     plt.axis('off')
                     plt.imshow(y_unnorm[j, :, :, :].cpu().numpy().transpose(1, 2, 0))
-                    plt.savefig(f'neurips_plots/ssim/masked_{fig_count}.png', bbox_inches='tight', dpi=300)
+                    plt.savefig(f'neurips_plots/lpips/masked_{fig_count}.png', bbox_inches='tight', dpi=300)
                     plt.close(fig)
 
                     # fig, ax1 = plt.subplots(1, 1)
@@ -162,21 +162,21 @@ def get_plots(args, G_ours, G_comod, test_loader, truncation, truncation_latent)
                         #     ax.set_xlabel('Ours',fontweight='bold')
                         ax.imshow(gens_ours[j, r, :, :, :].cpu().numpy().transpose(1, 2, 0))
 
-                    plt.savefig(f'neurips_plots/ssim/5_recons_ours_{fig_count}.png',bbox_inches='tight', dpi=300)
+                    plt.savefig(f'neurips_plots/lpips/5_recons_ours_{fig_count}.png',bbox_inches='tight', dpi=300)
                     plt.close(fig)
 
                     fig = plt.figure()
                     fig.subplots_adjust(wspace=0, hspace=0.05)
 
                     for r in range(5):
-                        ax = fig.add_subplot(5, 1, r+1)
+                        ax = fig.add_subplot(1, 5, r+1)
                         ax.set_xticks([])
                         ax.set_yticks([])
                         # if r == 2:
                         #     ax.set_xlabel('CoModGAN',fontweight='bold')
                         ax.imshow(gens_comod_psi_1[j, r, :, :, :].cpu().numpy().transpose(1, 2, 0))
 
-                    plt.savefig(f'neurips_plots/ssim/5_recons_comodgan_{fig_count}.png',bbox_inches='tight', dpi=300)
+                    plt.savefig(f'neurips_plots/lpips/5_recons_comodgan_{fig_count}.png',bbox_inches='tight', dpi=300)
                     plt.close(fig)
 
 
