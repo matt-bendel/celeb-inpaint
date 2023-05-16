3dca53f0a2c1e6f4542b0b79f2b21dbbffa8c9a8
Matt Bendel <mattbendel@Matts-MacBook-Pro-2.local>
2023-05-16 19:16:39 -0400

    plot change

--- evaluation_scripts/lpips/lpips_metric.py
+++ evaluation_scripts/lpips/lpips_metric.py
@@ -157,8 +157,8 @@ class LPIPSMetric:
                         samp_nums = [31, 30, 29, 26, 25, 24, 23, 21, 20, 18, 17, 16, 13 ,12 ,10, 8, 7]
                         subsamp_nums = [3, 6, 7, 8, 10, 12, 15, 16]
                         subsubsamp_nums = [0, 1, 2, 3, 7]
-                        for r in range(30):
-                            ax = fig.add_subplot(1, 30, tc)
+                        for r in [8,9,10,14,15,17,20,29,31]:
+                            ax = fig.add_subplot(1, 9, tc)
                             tc += 1
                             ax.set_xticks([])
                             ax.set_yticks([])
